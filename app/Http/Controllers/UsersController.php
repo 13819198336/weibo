bc70081c1234e259b1b7fb0ab92f696ece3973e7
vagrant <vagrant@homestead>
2020-03-29 16:14:03 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Routing\Route;
 
 class UsersController extends Controller
 {
@@ -14,4 +15,14 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request){
+        $this->Validate($request,[
+            'name'=>'required|unique:users|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6',
+        ]);
+        return;
+    }
+
 }
