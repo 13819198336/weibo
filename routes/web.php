b641bb8d1b2d0df8b251f1503aed866672baaca4
yangyejun1 <3001010396@qq.com>
2020-03-30 00:42:48 +0800

    完成用户注册功能

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('signup','UsersController@create')->name('signup');
 
 Route::resource('users','UsersController');
-//Route::get('/users/{user}','UsersController@show')->name('users.show');
+Route::get('/users/{user}','UsersController@show')->name('users.show');
